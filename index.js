5f09c11afeecfacf9df38631d72307b4ba9a4061
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 19:18:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(mus, instr) {
   var st = []
   for (var i = 0; i < mus.length; i++) {
-    st.push(`${mus} plays ${instr}`)
+    st.push(`${mus[i]} plays ${instr[i]}`)
   }
   return st
 }
